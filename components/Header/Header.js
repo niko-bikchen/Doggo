1b5ed1ad0f6a9ae695fa6c1199fd2c55830fbdaf
Nikolay Bikchentaev <niko.bikcheb@gmail.com>
2020-10-11 01:32:08 +0300

    Added initial navbar styles and logo

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,29 +1,55 @@
 import React from 'react';
 
-import './Header.module.css';
+import styles from './Header.module.css';
 
 import {
     MDBNavbar,
     MDBNavbarBrand,
     MDBNavbarNav,
     MDBNavItem,
-    MDBNavLink,
     MDBNavbarToggler,
     MDBCollapse,
     MDBFormInline,
     MDBDropdown,
     MDBDropdownToggle,
     MDBDropdownMenu,
-    MDBDropdownItem
+    MDBDropdownItem,
+    MDBBtn
 } from "mdbreact";
 
 const Header = () => {
     return (
-        <MDBNavbar color="#2B2B3B">
-            <MDBNavbarBrand>
-                <img className="Header-logo" src="Doggo_light.png" />
+        <MDBNavbar className={styles.Header} dark expand="md">
+            <MDBNavbarBrand className={styles['Header-brand']}>
+                <img className={styles['Header-logo']} src="Doggo_light.png" />
             </MDBNavbarBrand>
-            <MDBNavbarNav left></MDBNavbarNav>
+            <MDBNavbarNav left>
+                <MDBNavItem>
+                    <MDBDropdown>
+                        <MDBDropdownToggle nav caret>
+                            <div className="d-none d-md-inline">Послуги</div>
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu>
+                            <MDBDropdownItem href="#!">Вигул</MDBDropdownItem>
+                            <MDBDropdownItem href="#!">Обмін тварин</MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <span className="nav-link">Робота</span>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <MDBDropdown>
+                        <MDBDropdownToggle nav caret>
+                            <div className="d-none d-md-inline">Інформація</div>
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu>
+                            <MDBDropdownItem href="#!">Зони для вигулу</MDBDropdownItem>
+                            <MDBDropdownItem href="#!">Правила вигулу собак</MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+                </MDBNavItem>
+            </MDBNavbarNav>
             <MDBNavbarNav right></MDBNavbarNav>
         </MDBNavbar>
     );
