a5285e0cb78c664460b5adf550565bbf4f34050e
Nikolay Bikchentaev <niko.bikcheb@gmail.com>
2020-10-10 23:48:28 +0300

    Added PrimeReact

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-// pages/index.js
 import gql from 'graphql-tag';
 import React from "react";
-import {ApolloClient, InMemoryCache} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import withHttpsRedirect from "../HoCs/withHttpsRedirect";
-import {useState,useEffect, useRef, useCallback} from 'react'
-// import { getDataFromTree } from '@apollo/react-ssr'; useRef useEffect useImperativeHandle
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import { Menubar } from 'primereact/menubar';
 
 const QUERY = gql`
     query allCharacters {
@@ -22,15 +25,150 @@ export async function getStaticProps(context) {
         cache: new InMemoryCache()
     });
     const data = await client.query({
-        query:QUERY
+        query: QUERY
     })
     return {
-        props: {data:data.data}, // will be passed to the page component as props
+        props: { data: data.data }, // will be passed to the page component as props
     }
 }
+
+const items = [
+    {
+        label: 'File',
+        icon: 'pi pi-fw pi-file',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+                items: [
+                    {
+                        label: 'Bookmark',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label: 'Video',
+                        icon: 'pi pi-fw pi-video'
+                    },
+
+                ]
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Export',
+                icon: 'pi pi-fw pi-external-link'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            },
+
+        ]
+    },
+    {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-user-plus',
+
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-user-minus',
+
+            },
+            {
+                label: 'Search',
+                icon: 'pi pi-fw pi-users',
+                items: [
+                    {
+                        label: 'Filter',
+                        icon: 'pi pi-fw pi-filter',
+                        items: [
+                            {
+                                label: 'Print',
+                                icon: 'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'pi pi-fw pi-bars',
+                        label: 'List'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Events',
+        icon: 'pi pi-fw pi-calendar',
+        items: [
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {
+                        label: 'Save',
+                        icon: 'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    },
+
+                ]
+            },
+            {
+                label: 'Archieve',
+                icon: 'pi pi-fw pi-calendar-times',
+                items: [
+                    {
+                        label: 'Remove',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off'
+    }
+];
+
 //react hooks
-const Index = ({data}) => {
-    return JSON.stringify(data)
+const Index = ({ data }) => {
+    return (
+        <div className="Index">
+            <Menubar model={items} start={<div>Jopa</div>} />
+            <i className="pi pi-check"></i>
+        </div>
+    )
 };
 
 export default withHttpsRedirect(Index);
