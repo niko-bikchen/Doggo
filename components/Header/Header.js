f31a4ebd731f81f0ec5f0fa554c341c89e491ca1
BogdanVoylov <40231933+BogdanVoylov@users.noreply.github.com>
2020-10-20 23:23:50 +0300

    remade dogwalkingZones

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,10 +5,7 @@ import Link from "next/link";
 import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import DropdownBtn from "../DropdownBtn";
 import MenuItem from "@material-ui/core/MenuItem";
 
--- pages/dogwalkingZones.js
+++ pages/dogwalkingZones.js
@@ -4,10 +4,15 @@ import gql from "graphql-tag"
 import PageBase from "../components/PageBase/PageBase";
 
 import {NextSeo} from "next-seo";
-
+import { makeStyles } from '@material-ui/core/styles';
 import Map from "../components/Map"
 import _ from "underscore"
 import {Marker} from "@react-google-maps/api";
+import StyledTreeView, {StyledTreeItem} from "../components/StyledTreeView";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import RoomIcon from '@material-ui/icons/Room';
 
 const QUERY = gql`
     query{
@@ -32,7 +37,26 @@ export async function getStaticProps(ctx) {
 const LocationsToTree = React.memo(({locations, onAddressClick}) => {
     let count = 0;
 
-    return (<div></div>
+    return (
+        <StyledTreeView>
+            {_.values(_.mapObject(locations, (value, key) => {
+                return (
+                    <StyledTreeItem nodeId={key}  key={key} labelText={key} labelIcon={LocationCityIcon}>
+                        {value.map((el, i) =>{
+                            count++;
+                            return (
+                                <StyledTreeItem
+                                    onClick={onAddressClick({lat: el.lat, lng: el.lng})}
+                                    key={count+""}
+                                    nodeId={count+""}
+                                    labelText={el.address}
+                                    labelIcon={RoomIcon} />
+                            )
+                        })}
+                    </StyledTreeItem>
+                )
+            }))}
+        </StyledTreeView>
         /*<MDBTreeview
             theme='animated'
         >
@@ -57,7 +81,16 @@ const LocationsToTree = React.memo(({locations, onAddressClick}) => {
         </MDBTreeview>*/
     )
 }, () => true)
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 const DogwalkingZones = ({dogWalkingZones, dogwalkingZonesPageText}) => {
     const locations = _.groupBy(dogWalkingZones, 'district')
     const [currLocation, setCurrLocation] = useState({lat: dogWalkingZones[0].lat, lng: dogWalkingZones[0].lng})
@@ -67,17 +100,26 @@ const DogwalkingZones = ({dogWalkingZones, dogwalkingZonesPageText}) => {
             setCurrLocation({lng, lat})
         }
     }
+    const classes = useStyles();
     return (
         <PageBase background="Landing_body.jpg">
             <NextSeo canonical="https://doggo.co.ua/dogwalkingZones" title="Doggo | Места для выгула"/>
-            <div style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                padding: "40px",
-                alignItems: "center"
-            }}>
+                <Grid style={{padding:'20px'}} container spacing={3}>
+
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <LocationsToTree locations={locations} onAddressClick={onAddressClick}/>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper} style={{padding:"0"}}>
+                            <Map zoom={16} containerStyle={{width: "100%",borderRadius:"4px"}} center={currLocation}>
+                                <Marker position={currLocation}/>
+                            </Map>
+                        </Paper>
+                    </Grid>
+                </Grid>
+
                 {/*<MDBCard style={{width: "80vw"}}>
                     <MDBCardBody>
                         <MDBContainer>
@@ -100,7 +142,6 @@ const DogwalkingZones = ({dogWalkingZones, dogwalkingZonesPageText}) => {
 
                     </MDBCardBody>
                 </MDBCard>*/}
-            </div>
         </PageBase>
     )
 }
